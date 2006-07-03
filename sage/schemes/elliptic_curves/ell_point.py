307339b5da8bcac178780bd16ea3cabb737b8b35
William Stein <wstein@ucsd.edu>
2006-07-03 05:59:42 +0000

    [project @ doctests]

--- sage/schemes/elliptic_curves/ell_point.py
+++ sage/schemes/elliptic_curves/ell_point.py
@@ -165,9 +165,10 @@ class EllipticCurvePoint_field(SchemeMorphism_abelian_variety_coordinates_field)
             (5 : 5 : 1)
             sage: h = P.height()
             sage: RR = h.parent()
-            sage: RR.scientific_notation(True)
+            sage: RR.scientific_notation(True)    
             sage: h
-            -1.4246355279999999e-248
+            -1.4246355279999999e-248          # 32-bit
+            -3.0999021078264000e-691          # 64-bit
             sage: Q = 5*P
             sage: Q.height()
             0.0000000000000000e-1
