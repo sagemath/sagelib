74b3205f2c071775c4e4d6cc6066d968cef361f2
wstein@gmail.com <wstein@gmail.com>
2006-08-11 08:06:12 +0000

    [project @ matrix -- work in progress]

--- sage/matrix/matrix_pyx.pyx
+++ sage/matrix/matrix_pyx.pyx
@@ -6,7 +6,6 @@ AUTHORS:
     -- Martin Albrecht: conversion to Pyrex
     -- Jaap Spies: various functions
     -- Gary Zablackis: fixed a sign bug in generic determinant. 
-    
 
 Elements of matrix spaces are of class \code{Matrix} (or a class
 derived from Matrix).  They can be either sparse or dense, and can be
