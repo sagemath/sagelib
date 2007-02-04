a0cb9bdbec09a9b64babdaf3258f7a3cc866b728
William Stein <wstein@gmail.com>
2007-02-04 08:46:45 -0800

    linear algebra optimizations, etc.

--- sage/matrix/matrix_integer_dense_linbox.h
+++ sage/matrix/matrix_integer_dense_linbox.h
@@ -1,13 +1,9 @@
 #ifdef __cplusplus
-
-#endif
-
-#ifdef __cplusplus
 #define EXTERN extern "C"
 #else 
 #define EXTERN
 #endif
-
+ 
 /* linbox_minpoly allocates space for minpoly, so you have to call linbox_delete_array
    to free it up afterwards. */
 EXTERN void linbox_integer_dense_minpoly(mpz_t** minpoly, size_t* degree, 
@@ -19,3 +15,6 @@ EXTERN void linbox_integer_dense_delete_array(mpz_t* f);
 /* ans must be a pre-allocated and pre-initialized array of GMP ints. */
 EXTERN int linbox_integer_dense_matrix_matrix_multiply(mpz_t** ans, mpz_t **A, mpz_t **B,
 			      size_t A_nr, size_t A_nc, size_t B_nr, size_t B_nc);
+
+EXTERN void linbox_rational_dense_echelon_form(mpq_t** matrix, size_t nr, size_t nc);
+
