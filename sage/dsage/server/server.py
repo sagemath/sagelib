a96f4114b7af033f6e7e6d5ba6cee8feaeb9d41d
Yi Qiang <yqiang@gmail.com>
2007-03-22 11:04:18 -0700

    Added support for authenticated workers.

--- sage/dsage/server/server.py
+++ sage/dsage/server/server.py
@@ -335,6 +335,16 @@ class DSageServer(pb.Root):
             for h in hostinfo_list:
                 if h['uuid'] not in h.values():
                     hostinfo_list.append(h)
+    
+    def write_stats_to_xml(self):
+        r"""
+        Writes the server statistics to a XML file to be consumed by other 
+        programs
+        
+        """
+        
+        raise NotImplementedError
+        
 
 class DSageWorkerServer(DSageServer):
     r"""
