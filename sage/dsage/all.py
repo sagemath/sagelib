5b781e0e048adaca9a8b839c795034a16a6ceeff
Yi Qiang <yqiang@gmail.com>
2007-03-28 08:04:08 -0700

    More options to consider for how to import dist_functions.

--- sage/dsage/all.py
+++ sage/dsage/all.py
@@ -1,12 +1,16 @@
 """nodoctest
 """
 from sage.dsage.dsage import dsage
-#from sage.dsage.dist_functions.all import *
+from sage.dsage.dist_functions.all import *
+# from sage.dsage.dist_functions.dist_factor import DistributedFactor
 
 def DSage(server=None, port=8081, username=None, 
           pubkey_file=None, privkey_file=None):
     from sage.dsage.interface.dsage_interface import BlockingDSage
     return BlockingDSage(server=server, port=port, username=username,
                          pubkey_file=pubkey_file, privkey_file=privkey_file)
-    
+
+# def DistributedFactor(dsage, n):
+#     from sage.dsage.dist_functions.dist_factor import DistributedFactor
+#     return DistributedFactor(dsage, n)
 
