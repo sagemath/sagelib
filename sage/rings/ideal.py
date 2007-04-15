21eb3ff6a9014f2cbdbb9551382db610c5378448
William Stein <wstein@gmail.com>
2007-04-15 07:05:27 -0700

    Undo mistaken change to ideal.py

--- sage/rings/ideal.py
+++ sage/rings/ideal.py
@@ -137,14 +137,13 @@ def Ideal(R, gens=[], coerce=True):
         gens = [R(g) for g in gens]
 
     gens = list(set(gens))
-
     if isinstance(R, sage.rings.principal_ideal_domain.PrincipalIdealDomain):
         # Use GCD algorithm to obtain a principal ideal
         g = gens[0]
         for h in gens[1:]:
             g = R.gcd(g, h)
         return Ideal_pid(R, g)
-        
+
     if len(gens) == 1:
         return Ideal_principal(R, gens[0])
     
@@ -276,7 +275,7 @@ class Ideal_principal(Ideal_generic):
     def __init__(self, ring, gen):
         Ideal_generic.__init__(self, ring, [gen])
 
-    def _repr_(self):
+    def __repr__(self):
         return "Principal ideal (%s) of %s"%(self.gen(), self.ring())
 
     def is_principal(self):
@@ -349,7 +348,7 @@ class Ideal_pid(Ideal_principal):
 class Ideal_fractional(Ideal_generic):
     def __init__(self, ring, gen):
         Ideal_generic.__init__(self, ring, [gen])
-    def _repr_(self):
+    def __repr__(self):
         return "Fractional ideal %s of %s"%(self._repr_short(), self.ring())
     
 
