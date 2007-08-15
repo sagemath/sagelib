329710e101f7759c31f3f666b94b0fe6e5ba6768
'Martin Albrecht ext:(%27) <malb@informatik.uni-bremen.de>
2007-08-15 18:58:45 +0200

    set _zero/_one_element for RealField

--- sage/rings/real_mpfr.pyx
+++ sage/rings/real_mpfr.pyx
@@ -164,6 +164,8 @@ cdef class RealField(sage.rings.ring.Field):
     """
 
     def __init__(self, int prec=53, int sci_not=0, rnd="RNDN"):
+        cdef RealNumber rn
+
         if prec < MPFR_PREC_MIN or prec > MPFR_PREC_MAX:
             raise ValueError, "prec (=%s) must be >= %s and <= %s."%(
                 prec, MPFR_PREC_MIN, MPFR_PREC_MAX)
@@ -177,6 +179,21 @@ cdef class RealField(sage.rings.ring.Field):
         self.rnd = n
         self.rnd_str = rnd
         ParentWithGens.__init__(self, self, tuple([]), False)
+
+        # hack, we cannot call the constructor here
+        rn = PY_NEW(RealNumber)
+        rn._parent = self
+        mpfr_init2(rn.value, self.__prec)
+        rn.init = 1
+        mpfr_set_d(rn.value, 0.0, self.rnd)
+        self._zero_element = rn
+
+        rn = PY_NEW(RealNumber)
+        rn._parent = self
+        mpfr_init2(rn.value, self.__prec)
+        rn.init = 1
+        mpfr_set_d(rn.value, 1.0, self.rnd)
+        self._one_element = rn
 
     cdef RealNumber _new(self):
         """
@@ -529,7 +546,7 @@ cdef class RealNumber(sage.structure.element.RingElement):
         Return a new real number with same parent as self.
         """
         cdef RealNumber x
-        x = PY_NEW(RealNumber)
+        x = PY_NEW(RealNumber) 
         x._parent = self._parent
         mpfr_init2(x.value, (<RealField>self._parent).__prec)
         x.init = 1
