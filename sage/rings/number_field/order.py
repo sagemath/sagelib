80a392ac4a35608ffe34a957b9cfa2799509d139
William Stein <wstein@gmail.com>
2007-09-13 17:20:34 -0700

    Added code for computing all monomials...

--- sage/rings/number_field/order.py
+++ sage/rings/number_field/order.py
@@ -163,7 +163,9 @@ def each_is_integral(v):
             return False
     return True
 
-        
+
+from sage.rings.monomials import monomials
+
 def absolute_order_from_ring_generators(gens, check=True):
     """
     INPUT:
@@ -177,7 +179,7 @@ def absolute_order_from_ring_generators(gens, check=True):
     K = gens.universe()
     n = K.degree()
     
-    module_gens = []
+    module_gens = monomials(gens, n)
 
     
         
