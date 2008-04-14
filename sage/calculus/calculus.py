fc39451121f3674a8d3a7079cf666c5565e206b5
Mike Hansen <mhansen@gmail.com>
2008-04-14 13:38:32 -0700

    [mq]: 2915.patch

--- sage/calculus/calculus.py
+++ sage/calculus/calculus.py
@@ -2476,7 +2476,16 @@ class SymbolicExpression(RingElement):
 
         ALIASES:
             integral() and integrate() are the same.
-            
+
+
+        TESTS:
+            sage: a,b = var('a,b')
+            sage: integrate(1/(x^3 *(a+b*x)^(1/3)), x)
+            Traceback (most recent call last):
+            ...
+            TypeError: Computation failed since Maxima requested additional constraints (use assume):
+            Is  a  positive or negative?
+
         """
 
         if v is None:
