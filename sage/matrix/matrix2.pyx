b952cfcaff8dbe9130ec9467ab1cb58b0c258620
dfdeshom@sage <dfdeshom@gmail.com>
2008-04-30 11:08:44 -0700

    empty matrices: density() function throws a ZeroDivisionError

--- sage/matrix/matrix2.pyx
+++ sage/matrix/matrix2.pyx
@@ -3468,12 +3468,17 @@ cdef class Matrix(matrix1.Matrix):
             sage: A = matrix(QQ,3,3,[0,1,2,3,0,0,6,7,8])
             sage: A.density()
             2/3
-        
+
+            sage: a = matrix([[],[],[],[]])
+            sage: a.density()
+            0
         """
         cdef int x,y,k
         k = 0
         nr = self.nrows()
         nc = self.ncols()
+        if nc == 0 or nr == 0:
+            return 0
         for x from 0 <= x < nr:
             for y from 0 <= y < nc:
                 if not self.get_unsafe(x,y).is_zero():
