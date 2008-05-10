afb03368154053164389b25dc56028b532334d41
William Stein <wstein@gmail.com>
2008-05-10 13:27:51 -0700

    Fix trac #2636.  Pretty straightforward.

--- sage/server/notebook/cell.py
+++ sage/server/notebook/cell.py
@@ -454,7 +454,10 @@ class Cell(Cell_generic):
                 del self._interact_output
             except AttributeError:
                 pass
-            
+
+        # We have updated the input text so the cell can't have 
+        # been evaluated. 
+        self.__evaluated = False
         self.__version = 1+self.version()
         self.__in = input
         if hasattr(self, '_html_cache'):
