359385acdf8a444531575e3d6411d575b2fd801f
William Stein <wstein@gmail.com>
2008-10-30 13:38:50 -0700

    trac #4395 -- magma related optional doctest failure in sage/rings/quotient_ring.py

--- sage/rings/ring.pyx
+++ sage/rings/ring.pyx
@@ -1540,6 +1540,20 @@ cdef class FiniteField(Field):
         B = self.base_ring()
         p = self.polynomial()
         return "ext< %s | %s >"%(B._magma_init_(),p._magma_init_())
+
+    def _magma_convert_(self, magma):
+        """
+        Convert self to Magma.
+
+        EXAMPLES:
+            sage: magma(GF(9,'a'))           # optional -- requires magma
+            Finite field of size 3^2
+            sage: magma(GF(9,'a')).1         # optional -- requires magma
+            a
+        """
+        K = magma(self._magma_init_())
+        K.assign_names([self.variable_name()])
+        return K
     
     def _macaulay2_init_(self):
         """
