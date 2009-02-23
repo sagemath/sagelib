5e3bb988654bab4517832311d2586d4d683c8595
Gonzalo Tornaría <tornaria@math.utexas.edu>
2009-02-22 22:23:18 -0500

    #4470: many fixes and doctests for quadratic forms code

--- sage/quadratic_forms/quadratic_form__mass__Siegel_densities.py
+++ sage/quadratic_forms/quadratic_form__mass__Siegel_densities.py
@@ -139,6 +139,16 @@ def Pall_mass_density_at_odd_prime(self, p):
 
     OUTPUT:
         a rational number.
+
+    EXAMPLES:
+        sage: Q = QuadraticForm(ZZ, 3, [1,0,0,1,0,1])
+        sage: Q.Pall_mass_density_at_odd_prime(3)
+        [(0, Quadratic form in 3 variables over Integer Ring with coefficients: 
+        [ 1 0 0 ]
+        [ * 1 0 ]
+        [ * * 1 ]
+        )] [(0, 3, 8)] [8/9] 8/9
+        8/9
     """
     ## Check that p is a positive prime -- unnecessary since it's done implicitly in the next step. =)
     if p<=2:
