5a63ac6f42b63d28ef185f16b73714e75d9edaab
Mike Hansen <mhansen@gmail.com>
2009-05-19 16:11:30 -0700

    Updates for Pynac-0.1.7, main symbolics switch (#5930)

--- sage/rings/real_mpfr.pyx
+++ sage/rings/real_mpfr.pyx
@@ -1075,7 +1075,7 @@ cdef class RealNumber(sage.structure.element.RingElement):
             s = "%s \\times 10^{%s}" % (parts[0], parts[1])
         return s
 
-    def _interface_init_(self):
+    def _interface_init_(self, I=None):
         """
         Return string representation of self in base 10, avoiding
         scientific notation except for very large or very small numbers.
@@ -2923,9 +2923,9 @@ cdef class RealNumber(sage.structure.element.RingElement):
         
             sage: x, y = var('x,y')
             sage: 1.5^x
-            1.5^x
+            1.50000000000000^x
             sage: -2.3^(x+y^3+sin(x))
-            -2.3^(y^3 + sin(x) + x)
+            -2.30000000000000^(y^3 + x + sin(x))
         """
         cdef RealNumber x
         if not PY_TYPE_CHECK(self, RealNumber):
@@ -3411,6 +3411,19 @@ cdef class RealNumber(sage.structure.element.RingElement):
         _sig_off        
         return x
 
+    def arccoth(self):
+        """
+        Returns the inverse hyperbolic cotangent of this number
+        
+        EXAMPLES::
+        
+            sage: q = RR.pi()/5
+            sage: i = q.coth()
+            sage: i.arccoth() == q
+            True
+        """
+        return (~self).arctanh()
+
     def cot(self):
         """
         Returns the cotangent of this number
@@ -3442,6 +3455,19 @@ cdef class RealNumber(sage.structure.element.RingElement):
         mpfr_csch(x.value, self.value, (<RealField>self._parent).rnd)
         _sig_off        
         return x
+
+    def arccsch(self):
+        """
+        Returns the inverse hyperbolic cosecant of this number
+        
+        EXAMPLES::
+        
+            sage: i = RR.pi()/5
+            sage: q = i.csch()
+            sage: q.arccsch() == i
+            True
+        """
+        return (~self).arcsinh()
 
     def csc(self):
         """
@@ -3475,6 +3501,19 @@ cdef class RealNumber(sage.structure.element.RingElement):
         _sig_off        
         return x
 
+    def arcsech(self):
+        """
+        Returns the inverse hyperbolic secant of this number
+        
+        EXAMPLES::
+        
+            sage: i = RR.pi()/3
+            sage: q = i.sech()
+            sage: q.arcsech() == i
+            True
+        """
+        return (~self).arccosh()
+                
     def sec(self):
         """
         Returns the secant of this number
