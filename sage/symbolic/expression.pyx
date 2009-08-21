4362106154fa24f920a25a1d2785e123a270b777
Alexandru Ghitza <aghitza@alum.mit.edu>
2009-08-21 22:45:24 +1000

    fix doctest failures due to upgrade to Maxima 5.19.1

--- sage/symbolic/expression.pyx
+++ sage/symbolic/expression.pyx
@@ -2465,8 +2465,8 @@ cdef class Expression(CommutativeRingElement):
             sage: sin(x/2).expand_trig(half_angles=False)
             sin(1/2*x)
             sage: sin(x/2).expand_trig(half_angles=True)
-            1/2*sqrt(-cos(x) + 1)*sqrt(2)
-        
+            1/2*sqrt(-cos(x) + 1)*sqrt(2)*(-1)^floor(1/2*x/pi)
+
         ALIASES:
 
         :meth:`trig_expand` and :meth:`expand_trig` are the same
