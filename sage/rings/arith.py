3e61a61a3bed583315360604108b8547a90950f7
Yann Laigle-Chapuy <yannlaiglechapuy@gmail.com>
2010-09-22 16:16:03 +0200

    #9941 faster multinomial coefficients

--- sage/rings/arith.py
+++ sage/rings/arith.py
@@ -3144,110 +3144,91 @@ def binomial_coefficients(n):
         d[k, n-k] = d[n-k, k] = a
     return d
 
-def multinomial_coefficients(m, n, _tuple=tuple, _zip=zip):
+def multinomial_coefficients(m, n):
     r"""
     Return a dictionary containing pairs
     `\{(k_1,k_2,...,k_m) : C_{k,n}\}` where
     `C_{k,n}` are multinomial coefficients such that
     `n = k_1 + k_2 + ...+ k_m`.
-    
+
     INPUT:
-    
-    
+
     -  ``m`` - integer
-    
     -  ``n`` - integer
-    
-    -  ``_tuple, _zip`` - hacks for speed; don't set
-       these as a user.
-    
-    
+
     OUTPUT: dict
-    
+
     EXAMPLES::
-    
+
         sage: sorted(multinomial_coefficients(2,5).items())
         [((0, 5), 1), ((1, 4), 5), ((2, 3), 10), ((3, 2), 10), ((4, 1), 5), ((5, 0), 1)]
-    
+
     Notice that these are the coefficients of `(x+y)^5`::
-    
+
         sage: R.<x,y> = QQ[]
         sage: (x+y)^5
         x^5 + 5*x^4*y + 10*x^3*y^2 + 10*x^2*y^3 + 5*x*y^4 + y^5
-    
+
     ::
-    
+
         sage: sorted(multinomial_coefficients(3,2).items())
         [((0, 0, 2), 1), ((0, 1, 1), 2), ((0, 2, 0), 1), ((1, 0, 1), 2), ((1, 1, 0), 2), ((2, 0, 0), 1)]
-    
+
     ALGORITHM: The algorithm we implement for computing the multinomial
     coefficients is based on the following result:
-    
-    Consider a polynomial and its `n`-th exponent:
-    
-    
-    .. math::
-    
-         P(x) = \sum_{i=0}^m p_i x^k 
-    
-    
-    
-    
-    .. math::
-    
-         P(x)^n = \sum_{k=0}^{m n} a(n,k) x^k 
-    
-    
-    
-    We compute the coefficients `a(n,k)` using the J.C.P.
-    Miller Pure Recurrence [see D.E.Knuth, Seminumerical Algorithms,
-    The art of Computer Programming v.2, Addison Wesley, Reading,
-    1981].
-    
-    .. math::
-    
-                  a(n,k) = 1/(k p_0) \sum_{i=1}^m p_i ((n+1)i-k) a(n,k-i),        
-    
-    
-    where `a(n,0) = p_0^n`.
-    
-    AUTHORS:
 
-    - Pearu Peterson
+    ..math::
+
+        \binom{n}{k_1, \cdots, k_m} =
+        \frac{k_1+1}{n-k_1}\sum_{i=2}^m \binom{n}{k_1+1, \cdots, k_i-1, \cdots}
+
+    e.g.::
+
+        sage: k = (2, 4, 1, 0, 2, 6, 0, 0, 3, 5, 7, 1) # random value
+        sage: n = sum(k)
+        sage: s = 0
+        sage: for i in range(1, len(k)):
+        ...       ki = list(k)
+        ...       ki[0] += 1
+        ...       ki[i] -= 1
+        ...       s += multinomial(n, *ki)
+        sage: multinomial(n, *k) == (k[0] + 1) / (n - k[0]) * s
+        True
     """
     if m == 2:
         return binomial_coefficients(n)
-    symbols = [(0,)*i + (1,) + (0,)*(m-i-1) for i in range(m)]
-    s0 = symbols[0]
-    p0 = [_tuple(aa-bb for aa,bb in _zip(s,s0)) for s in symbols]
-    r = {_tuple(aa*n for aa in s0):1}
-    r_get = r.get
-    r_update = r.update
-    l = [0] * (n*(m-1)+1)
-    l[0] = r.items()
-    for k in xrange(1, n*(m-1)+1):
-        d = {}
-        d_get = d.get
-        for i in xrange(1, min(m,k+1)):
-            nn = (n+1)*i-k
-            if not nn:
-                continue
-            t = p0[i]
-            for t2, c2 in l[k-i]:
-                tt = _tuple([aa+bb for aa,bb in _zip(t2,t)])
-                cc = nn * c2
-                b = d_get(tt)
-                if b is None:
-                    d[tt] = cc
-                else:
-                    cc = b + cc
-                    if cc:
-                        d[tt] = cc
-                    else:
-                        del d[tt]
-        r1 = [(t, c//k) for (t, c) in d.iteritems()]
-        l[k] = r1
-        r_update(r1)
+    t = [n] + [0] * (m - 1)
+    r = {tuple(t): 1}
+    if n:
+        j = 0 # j will be the leftmost nonzero position
+    else:
+        j = m
+    # enumerate tuples in co-lex order
+    while j < m - 1:
+        # compute next tuple
+        tj = t[j]
+        if j:
+            t[j] = 0
+            t[0] = tj
+        if tj > 1:
+            t[j + 1] += 1
+            j = 0
+            start = 1
+            v = 0
+        else:
+            j += 1
+            start = j + 1
+            v = r[tuple(t)]
+            t[j] += 1
+        # compute the value
+        # NB: the very first step was mixed above
+        for k in xrange(start, m):
+            if t[k]:
+                t[k] -= 1
+                v += r[tuple(t)]
+                t[k] += 1
+        t[0] -= 1
+        r[tuple(t)] = (v * tj) // (n - t[0])
     return r
 
 
