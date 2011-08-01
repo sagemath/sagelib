945cadd2c80787641638483186856590a8cb3453
R. Andrew Ohana <andrew.ohana@gmail.com>
2011-08-01 16:58:55 -0700

    Trac #11640: 11640 - Removed references of DB_HOME in preference of SAGE_DATA

--- sage/databases/db.py
+++ sage/databases/db.py
@@ -93,17 +93,15 @@ Gallileus GmbH                   http://www.gallileus.info/
 import os, shutil, cPickle
 
 import sage.databases.compressed_storage
-import sage.misc.misc
+from sage.misc.misc import SAGE_DATA
 
-# The following effectively turns off the ZODB logger, which is OK for us. 
+# The following effectively turns off the ZODB logger, which is OK for us.
 # Without this, one gets this annoying error message a lot:
 #       No handlers could be found for logger "ZODB.FileStorage"
 import logging
 logging.getLogger("ZODB.FileStorage").setLevel(10000000)
 logging.getLogger("ZODB.lock_file").setLevel(10000000)
-logging.getLogger("ZODB.Connection").setLevel(10000000) 
-
-DB_HOME = "%s/data/"%sage.misc.misc.SAGE_ROOT
+logging.getLogger("ZODB.Connection").setLevel(10000000)
 
 class _uniq(object):
     _db = {} # Class variable, no globals!
@@ -130,12 +128,12 @@ class Database(_uniq):
         name = self.name
         read_only = self.read_only
         thresh = self._thresh
-        if not os.path.exists("%s/%s"%(DB_HOME,name)):
+        if not os.path.exists("%s/%s"%(SAGE_DATA,name)):
             try:
-                os.makedirs("%s/%s"%(DB_HOME,name))
+                os.makedirs("%s/%s"%(SAGE_DATA,name))
             except OSError:    # for online calculator...
                 pass
-        self._dbname = "%s/%s/%s"%(DB_HOME, name, name)
+        self._dbname = "%s/%s/%s"%(SAGE_DATA, name, name)
         if self.read_only and not os.path.exists(self._dbname):
             raise RuntimeError, "The database %s is not installed."%self._dbname
         fs = FileStorage.FileStorage(self._dbname, read_only=self.read_only)
@@ -180,7 +178,7 @@ class Database(_uniq):
         calling changed to do that).
         """
         self.root._p_changed = 1
-        X = self.root[key]        
+        X = self.root[key]
         self.root[key] = X
 
     def pack(self):
@@ -292,7 +290,7 @@ class Database(_uniq):
         X = self.as_dict(keys)
         print "Dumping %s..."%filename
         s = cPickle.dumps(X,2)
-        dir = "%s/pickles/"%DB_HOME
+        dir = "%s/pickles/"%SAGE_DATA
         if not os.path.exists(dir):
             os.makedirs(dir)
         open("%s/%s"%(dir,filename), "w").write(s)
@@ -313,7 +311,7 @@ class Database(_uniq):
         """
         if self.read_only:
             raise RuntimeError, "%s is read only."%self
-        dir = "%s/pickles/"%DB_HOME
+        dir = "%s/pickles/"%SAGE_DATA
         s = open("%s/%s"%(dir,filename)).read()
         print "Restoring %s..."%filename
         X = cPickle.loads(s,2)
@@ -328,7 +326,7 @@ class Database(_uniq):
         Each file is loaded then committed to disk before the next
         file is loaded. 
         """
-        X = os.listdir("%s/pickles/"%DB_HOME)
+        X = os.listdir("%s/pickles/"%SAGE_DATA)
         n = len(basename)
         for F in X:
             if F[:n] == basename:
@@ -349,7 +347,7 @@ class Database(_uniq):
         There must not be a database with the new_name already, or a
         RuntimeError exception is raised.
         """
-        if os.path.exists("%s/%s"%(DB_HOME,new_name)):
+        if os.path.exists("%s/%s"%(SAGE_DATA,new_name)):
             raise RuntimeError, "Cannot clone to %s since that database already exists."%name
-        os.path.makedirs("%s/%s"%(DB_HOME,new_name))
-        shutil.copy2("%s/%s/%s"%(DB_HOME,name,name), "%s/%s"%(DB_HOME,new_name))
+        os.path.makedirs("%s/%s"%(SAGE_DATA,new_name))
+        shutil.copy2("%s/%s/%s"%(SAGE_DATA,name,name), "%s/%s"%(SAGE_DATA,new_name))
